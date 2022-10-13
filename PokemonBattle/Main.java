d3abf7b3eff6ecb668d2edc3fe4278bc2c0bb472
TominKapp <81731981+TominKapp@users.noreply.github.com>
2022-10-12 22:56:01 -0700

    Update Main.java

--- PokemonBattle/Main.java
+++ PokemonBattle/Main.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
     Pokedex dex = new Pokedex();
     
-    Pokemon a = dex.createPokemon(18, 50);
-    Pokemon b = dex.createPokemon(20, 50);
+    System.out.print("Enter your Pokemon ID (<151):");
+    int yourPoke = s.nextInt();
+    if (yourPoke > 151) yourPoke = 151;
+    
+    System.out.print("Enter enemy Pokemon ID (<151):");
+    int enemyPoke = s.nextInt();
+    if (enemyPoke > 151) enemyPoke = 151;
+    
+    Pokemon a = dex.createPokemon(yourPoke, 50);
+    Pokemon b = dex.createPokemon(enemyPoke, 50);
     
     MyFrame mf = new MyFrame(a, b);
     mf.display();
